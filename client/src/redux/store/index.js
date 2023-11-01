f1ecb6e3a4ea94c06b931cac9d976db11f7bc277
bautistax <jbautistabauza@gmail.com>
2023-11-01 18:22:42 -0300

    dependecies

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
 const composeEnhancers =
-    (typeof window !== undefined && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
+    (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
